7a28be42108f1feab3b22118875e02ef34365aee
MweleCarol <mwelecarolyne65@gmail.com>
2026-03-26 13:07:18 +0300

    Implement cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import Cocktails from './Components/Cocktails';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
     <main>
       <Navbar/>
       <Hero/>
-      
+      <Cocktails/>
     </main>
   );
 }
---
+++ src/Components/Cocktails.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { cocktailLists, mockTailLists } from '../../Constants';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const Cocktails = () => {
+
+    useGSAP(()=>{
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger:{
+                trigger: '#cocktails',
+                start: 'top 30%',
+                end:'bottom 80%',
+                scrub: true,
+            }
+        })
+        parallaxTimeline
+         .from('#c-left-leaf',{
+          x: -100, y: 100
+        })
+        .from('#c-right-leaf',{
+          x: 100, y: 100
+        })
+    })
+
+  return (
+    <section id='cocktails' className='noisy'>
+      <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id='c-left-leaf' />
+      <img src="/images/cocktail-right-leaf.png" alt="r-leef" id='c-right-leaf' />
+      <div className="list">
+        <div className="popular">
+            <h2>Most popular cocktails</h2>
+            <ul>
+                {cocktailLists.map((drink)=>( //In stead of repeating drink you can just list what you wnat from it instead
+                   <li key={drink.name}>
+                    <div className="md:me-28">
+                        <h3>{drink.name}</h3>
+                        <p>{drink.country} | {drink.detail}</p>
+                    </div>
+                    <span>- {drink.price}</span>
+                   </li> 
+                ))}
+            </ul>
+        </div>
+
+        <div className="popular">
+            <h2>Most popular mocktails</h2>
+            <ul>
+                {mockTailLists.map((drink)=>( //In stead of repeating drink you can just list what you wnat from it instead
+                   <li key={drink.name}>
+                    <div className="me-28">
+                        <h3>{drink.name}</h3>
+                        <p>{drink.country} | {drink.detail}</p>
+                    </div>
+                    <span>- {drink.price}</span>
+                   </li> 
+                ))}
+            </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Cocktails;
+
