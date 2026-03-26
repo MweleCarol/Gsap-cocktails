7063350a3ab7de97a192d6d15bb3b638fc8187f1
MweleCarol <mwelecarolyne65@gmail.com>
2026-03-26 18:40:24 +0300

    Final commit of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { ScrollTrigger, SplitText } from 'gsap/all';
-import gsap from 'gsap';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import Cocktails from './Components/Cocktails';
-import About from './Components/About';
-import Art from './Components/Art';
-import Menu from './Components/Menu';
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Cocktails from "./Components/Cocktails";
+import About from "./Components/About";
+import Art from "./Components/Art";
+import Menu from "./Components/Menu";
+import Contact from "./Components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Hero/>
-      <Cocktails/>
-      <About/>
-      <Art/>
-      <Menu/>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
-}
+};
 
 export default App;
